b816381abd72da913f31f89de47935b51637ce09
computeIt <pushok@mail.ru>
2017-03-25 14:29:39 +0300

    Update CurrencyRatesRestClientTestApp.java

--- CurrencyRatesRestClientTestApp.java
+++ CurrencyRatesRestClientTestApp.java
@@ -30,11 +30,19 @@ public class CurrencyRatesRestClientTestApp {
 		String avCur = getAvailableCurrencies(response);
 		System.out.println("available currencies: " + avCur);
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-		
+		String dateRequest = "";
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		
 		while(true){
 			try{
+				dateRequest = "latest";
+				System.out.println("if you need archive info type Y ");
+				String choice = reader.readLine();
+				if(choice.equalsIgnoreCase("y")){
+					System.out.println("input date in format YYYY-MM-DD");
+					dateRequest = reader.readLine();					
+				}
+				
 				System.out.println("choose base currency (3 BIG letters) or type EXIT");				
 				String baseCurrency = reader.readLine();
 				if(baseCurrency.equalsIgnoreCase("exit")){
@@ -62,7 +70,7 @@ public class CurrencyRatesRestClientTestApp {
 				if(!avCur.contains(destCurrency))
 					throw new IllegalArgumentException();
 				
-				url = "http://api.fixer.io/latest?base=" + baseCurrency;
+				url = "http://api.fixer.io/" + dateRequest + "?base=" + baseCurrency;
 				response = restTemplate.exchange(url, HttpMethod.GET, request, new ParameterizedTypeReference<Info>()	{	});
 				
 				double result = Double.parseDouble(response.getBody().getRates().get(destCurrency)) * quantity;
@@ -80,6 +88,7 @@ public class CurrencyRatesRestClientTestApp {
 		for(String s: response.getBody().getRates().keySet()){
 			list.add(s);			
 		}
+		list.add("EUR");
 		return list.toString();
 	}
 }
