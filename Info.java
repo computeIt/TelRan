4feebddcf1944965efe6dd9f82335b1b800e0b2b
computeIt <pushok@mail.ru>
2017-03-25 13:21:35 +0300

    Create Info.java

---
+++ Info.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Info {
+	public String base;
+	public Date date;
+	public Map<String, String> rates;
+	
+	public String getBase() {
+		return base;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public Map<String, String> getRates() {
+		return rates;
+	}	
+}
